91a67bb08263c6e1bbcfb06f3c64ae459450d959
Anupam Jain <ajnsit@gmail.com>
2019-10-24 16:59:44 +0530

    Include fpncr logo in the bundle, so it's built and cached by parcel

---
+++ src/Pages/Home.js
@@ -0,0 +1,3 @@
+'use strict';
+
+exports.fpncrLogoPath = require('../../static/fpncr-logo.png');
--- src/Pages/Home.purs
+++ src/Pages/Home.purs
@@ -6,9 +6,11 @@ import Concur.React.MUI.DOM as MD
 import Concur.React.DOM as D
 import Concur.React.Props as P
 
+foreign import fpncrLogoPath :: String
+
 homePage :: forall a. Widget HTML a
 homePage = MD.box []
-  [ D.p [P.unsafeMkProp "align" "center"] [D.img [P.height "50%", P.src "fpncr-logo.png", P.alt "Functional Programming NCR's cover image"]]
+  [ D.p [P.unsafeMkProp "align" "center"] [D.img [P.height "50%", P.src fpncrLogoPath, P.alt "Functional Programming NCR's cover image"]]
   , D.h2 [] [D.text "About Us"]
   , D.p [] [D.text "We are a community and a meetup group for all Haskellers / other Functional Programming language enthusiasts in and around the Delhi NCR area. You can join and participate in the online events even if you are somewhere else. All skill levels from novices to gods of category theory are welcome."]
   , D.h2 [] [D.text "How To Connect"]
